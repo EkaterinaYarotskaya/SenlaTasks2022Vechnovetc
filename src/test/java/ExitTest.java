77a816524e4e8e1bafa200a1f1d32965dacd94f5
Sergei Shabalin <SShabalin@alfabank.ru>
2022-03-07 23:51:57 +0300

    На исправление 07.03.22

--- src/test/java/ExitTest.java
+++ src/test/java/ExitTest.java
@@ -7,6 +7,7 @@ public class ExitTest extends FixedData {
 
     //такого рода данные типо логин, пароль, URL обычно пишут в файл с расширением .property, читай ссылку ниже
     // https://devcolibri.com/%D0%BF%D1%80%D0%BE%D1%81%D1%82%D0%BE%D0%B9-%D0%BF%D1%80%D0%B8%D0%BC%D0%B5%D1%80-%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D1%8B-%D1%81-property-%D1%84%D0%B0%D0%B9%D0%BB%D0%B0%D0%BC%D0%B8-%D0%B2-java/
+    //Так же поменял версию хромдрайвера на 99
     String CORRECT_LOGIN = "tomsmith";
     String CORRECT_PASSWORD = "SuperSecretPassword!";
 
